6b5747aae9dcf5b95b865deb4418ab427bc75a10
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-09-11 15:07:00 +0200

    Create cw_11_09_2024.py

---
+++ Klasa3/cw_11_09_2024.py
@@ -0,0 +1,83 @@
+# Rozklad na czynniki pierwsze
+# n = int(input("Podaj liczbe do rokladu: "))
+# czynnik = 2
+# while n > 1:
+#    while n % czynnik == 0:
+#        print(czynnik, end=" ")
+#        n = n//czynnik
+#    czynnik += 1
+
+# Zad.1
+# n = int(input("Podaj liczbe: "))
+# czynnik = 2
+# while n >1:
+#     while n % czynnik == 0:
+#         print(czynnik)
+#         n = n//czynnik
+#     czynnik += 1
+
+# Zad.2
+# n = int(input("Podaj liczbe: "))
+# czynnik = 2
+# suma = 0
+# while n >1:
+#     while n % czynnik == 0:
+#         suma += czynnik
+#         n = n//czynnik
+#     czynnik += 1
+# print(suma)
+
+# Zad.3
+# n = int(input("Podaj liczbe: "))
+# czynnik = 2
+# suma = 0
+# temp = 1
+# while n >1:
+#     while n % czynnik == 0:
+#         suma += czynnik
+#         n = n//czynnik
+#     czynnik += 1
+# for i in range(2,suma):
+#     if suma % i == 0:
+#         print("NIE")
+#         temp = 0
+# if temp == 1:
+#     print("TAK")
+
+# Zad. 4
+# def czyJestNaLiscie(lista, x):
+#     for i in lista:
+#         if i == x:
+#             return 0
+#     return 1
+# n = int(input("Podaj liczbe do rokladu: "))
+# lista = []
+# czynnik = 2
+# while n > 1:
+#    while n % czynnik == 0:
+#        if czyJestNaLiscie(lista, czynnik):
+#            lista.append(czynnik)
+#        n = n//czynnik
+#    czynnik += 1
+# for i in lista:
+#     print(i, end=" ")
+
+# Zad. 5
+# def sumaCyfr(n):
+#     suma=0
+#     while n>0:
+#         suma=suma+n%10
+#         n=n//10
+#     return suma
+# n = int(input("Podaj liczbe: "))
+# czynnik = 2
+# suma = 0
+# while n >1:
+#     while n % czynnik == 0:
+#         suma += czynnik
+#         n = n//czynnik
+#     czynnik += 1
+# if sumaCyfr(n)==sumaCyfr(suma):
+#     print("TAK")
+# else:
+#     print("NIE")
