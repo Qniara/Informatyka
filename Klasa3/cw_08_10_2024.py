9d972f40c1ec3ec7b6f2f1276bf48ca89fbab0f2
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-10-09 15:05:55 +0200

    Update cw_08_10_2024.py

--- Klasa3/cw_08_10_2024.py
+++ Klasa3/cw_08_10_2024.py
@@ -1,26 +1,28 @@
 def Wyszukiwanie_Binarne(T, a, n):
-    lewy=0
-    prawy=n-1
+    lewy = 0
+    prawy = n - 1
     while lewy < prawy:
-        srodek=(lewy+prawy)//2
-        if T[srodek]<a:
-            lewy=srodek+1
+        srodek = (lewy + prawy) // 2
+        if T[srodek] < a:
+            lewy = srodek + 1
         else:
-            prawy=srodek
-    return T[lewy]==a
+            prawy = srodek
+    return T[lewy] == a
 
-def Wyszukiwanie_Binarne_rek(T, a, lewy,prawy):
+
+def Wyszukiwanie_Binarne_rek(T, a, lewy, prawy):
     if lewy < prawy:
-        srodek=(lewy+prawy)//2
-        if T[srodek]<a:
-            return Wyszukiwanie_Binarne_rek(T,a,srodek+1,prawy)
+        srodek = (lewy + prawy) // 2
+        if T[srodek] < a:
+            return Wyszukiwanie_Binarne_rek(T, a, srodek + 1, prawy)
         else:
-            return Wyszukiwanie_Binarne_rek(T,a,lewy,srodek)
-    return T[lewy]==a
+            return Wyszukiwanie_Binarne_rek(T, a, lewy, srodek)
+    return T[lewy] == a
 
-def CzyNiemalejacy(T,n):
-    for i in range(n-1):
-        if T[i]>T[i+1]:
+
+def CzyNiemalejacy(T, n):
+    for i in range(n - 1):
+        if T[i] > T[i + 1]:
             return False
     return True
 
@@ -45,7 +47,7 @@ def CzyNiemalejacy(T,n):
 # def przetwarzaj_pliki(nazwa_pliku):
 #     with open(nazwa_pliku, 'r') as plik:
 #         liczba_ciagow = int(plik.readline().strip())
-        
+
 #         for _ in range(liczba_ciagow):
 #             liczba_elementow = int(plik.readline().strip())
 #             ciag = list(map(int, plik.readline().strip().split()))
@@ -54,19 +56,29 @@ def CzyNiemalejacy(T,n):
 #                 print("Ciąg zawierający liczbę 10:", ciag)
 # przetwarzaj_pliki('ciagi2.txt')
 
+# Wersja Śledź
+# plik=open("ciagi2.txt", "r")
+# n=int(plik.readline().rstrip())
+# for i in range(n):
+#     d=int(plik.readline().rstrip())
+#     ciag = list(map(int, plik.readline().split()))
+#     if Wyszukiwanie_Binarne(ciag, 10, d):
+#         print(ciag)
+# plik.close()
+
 # Zad.5
 # Funkcja wyszukiwanie_binarne(T, a, n):
-#     lewy ← 1      
-#     prawy ← n           
+#     lewy ← 1
+#     prawy ← n
 #     dopóki lewy ≤ prawy wykonuj:
 #         srodek ← (lewy + prawy) div 2
-
 #         jeżeli T[srodek] = a to:
 #             zwróć Prawda
 #         w przeciwnym razie jeżeli T[srodek] < a to:
-#             lewy ← srodek + 1   
-#         w przeciwnym razie:      
-#             prawy ← srodek - 1   
+#             lewy ← srodek + 1
+#         w przeciwnym razie:
+#             prawy ← srodek - 1
 
-#     zwróć Fałsz                      
+#     zwróć Fałsz
 
+# Zad.6
