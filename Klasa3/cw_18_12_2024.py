e06e03da338734f88d9a863b5b966275f31aa231
Ziemniak <114557943+Qniara@users.noreply.github.com>
2024-12-18 15:01:23 +0100

    Create cw_18_12_2024.py

---
+++ Klasa3/cw_18_12_2024.py
@@ -0,0 +1,18 @@
+# Zadanie maturalne
+# Zad.3.4
+# plik = open("pi_przyklad.txt","r")
+# ciag= list(map(int, plik.read().split()))
+# plik.close()
+# czyRosnie = True
+# fragment = [ciag[0]]
+# n=len(ciag)
+# for i in range(1,n):
+#     if czyRosnie and ciag[i] > fragment[-1]:
+#         fragent.append(ciag[i])
+#     else:
+#         czyRosnie= False
+#         fragment.append(ciag[i])
+#     if !czyRosnie and ciag[i]<fragment[-1]:
+#         fragment.append(ciag[i])
+#     else:
+#
